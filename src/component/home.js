f5441cce19e73203216576022447ba865d4226d2
pierre schuvey <chromatykretro@gmail.com>
2024-07-02 10:35:08 +0200

    home

--- src/component/home.js
+++ src/component/home.js
@@ -4,13 +4,13 @@ import {Axios} from "axios";
 
 function HomePage(props) {
     useEffect(() => {
-        fetch("https://karlia.fr/app/api/v2/documents", {
-            method: 'get',
-            headers: new Headers({
-                'Authorization': 'Bearer e4e05o-8z7n8e-144030-zvpmas-nhonth',
-                'Content-Type': 'application/x-www-form-urlencoded'
-            })
-        }).then(res => res.json());
+        fetch("https://karlia.fr/app/api/v2/documents",
+            {
+                    'Authorization': 'bearer e4e05o-8z7n8e-144030-zvpmas-nhonth',
+                    'Content-Type': 'application/json'
+                }
+        )
+        .then(res => res.json());
     }, []);
     return (
         <>
