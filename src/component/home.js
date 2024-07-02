fafce1dde663007757df049e0d0d3d9e51709a66
pierre schuvey <chromatykretro@gmail.com>
2024-07-02 11:37:13 +0200

    fix

--- src/component/home.js
+++ src/component/home.js
@@ -4,15 +4,8 @@ import {Axios} from "axios";
 
 function HomePage(props) {
     useEffect(() => {
-        fetch('https://karlia.fr/app/api/v2/documents', {
-            method: 'get',
-            headers: new Headers({
-                'Access-Control-Allow-Origin':'*',
-                'method': 'GET',
-                'Authorization': 'Bearer e4e05o-8z7n8e-144030-zvpmas-nhonth',
-                'Content-Type': 'application/json'
-            })
-        });
+        Axios
+            .get(`/api/documents`,{headers: { Authorization: `Bearer e4e05o-8z7n8e-144030-zvpmas-nhonth` }})
     }, []);
     return (
         <>
