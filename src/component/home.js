6c9edbecb3ebc45a479f9f33a5c7951023a4bc0a
pierre schuvey <chromatykretro@gmail.com>
2024-07-02 11:09:14 +0200

    home

--- src/component/home.js
+++ src/component/home.js
@@ -7,6 +7,7 @@ function HomePage(props) {
         fetch('https://karlia.fr/app/api/v2/documents', {
             method: 'get',
             headers: new Headers({
+                'Method': 'GET',
                 'Authorization': 'Bearer e4e05o-8z7n8e-144030-zvpmas-nhonth',
                 'Content-Type': 'application/json'
             })
