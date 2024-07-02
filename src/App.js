a43e9ad25d56b9e4dc3de730ada34c80bb56e304
pierre schuvey <chromatykretro@gmail.com>
2024-07-02 09:20:51 +0200

    home

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React,{useState, useEffect} from 'react';
+import React from 'react';
 import { BrowserRouter, Route, Routes} from "react-router-dom";
-import { useCookies } from 'react-cookie';
-import $ from "jquery";
 import './App.css';
-import 'react-tooltip/dist/react-tooltip.css'
 import HomePage from './component/home.js';
-function App(props) {
-  const [cookies, setCookie] = useCookies();
-  if(Object.keys(cookies).length == 0) {
-    return <Login />
-  }
+function App() {
   return(
       <>
-        {cookies.user !== undefined &&
             <BrowserRouter>
-              <NavBar cookies={cookies} />
               <Routes>
                 <Route path="/" element={<HomePage />} />
               </Routes>
               {/*<Footer cookies={cookies} />*/}
             </BrowserRouter>
-        }
       </>
   );
 }
