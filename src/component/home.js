b1b1bfddf143df6cbc2559a7d1952fef765dd39c
pierre schuvey <chromatykretro@gmail.com>
2024-07-02 12:48:42 +0200

    fix

--- src/component/home.js
+++ src/component/home.js
@@ -27,6 +27,12 @@ function HomePage(props) {
                         }
                 }
             )
+                .then(function(response){
+                    Axios.get(`/api/documents?type=4`,{headers: { Authorization: `Bearer e4e05o-8z7n8e-144030-zvpmas-nhonth` }})
+                        .then(function(response){
+                            setFactures(response.data.data);
+                        })
+                })
         })
     };
     console.log(selected);
