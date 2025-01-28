38cb003050b909587c6ad7f1ac678683ce429e5b
pierre schuvey <chromatykretro@gmail.com>
2025-01-28 10:14:13 +0100

    fix

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -7,11 +7,13 @@ const api = express();
 const router = Router();
 router.get("/hello", (req, res) => res.send("Hello World!"));
 router.get("/getCompaniesHubspot", (req, res, next)=>{
+
     axios.get(
         'https://api.hubapi.com /crm/v3/objects/companies',
         {
             headers: {
-                Authorization: `Bearer ${process.env.REACT_APP_CLIENT_SECRET}`
+                Authorization: `Bearer pat-na1-2ae0381d-ef96-41d8-8e60-81da0ac163a0`,
+                'Content-Type': 'application/json',
             },
         }
     ).then(response => res.json(response.data))
