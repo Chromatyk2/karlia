1bc51238cce52b350a2fcb08be09ff7132ed1a09
pierre schuvey <chromatykretro@gmail.com>
2025-03-06 14:43:13 +0100

    fix

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -8,30 +8,10 @@ const router = Router();
 router.get("/hello", (req, res) => res.send("Hello World!"));
 router.get("/createCompanie", (req, res, next)=>{
     Axios.post(
-        'https://api.hubapi.com/crm/v3/objects/companies',
+        'https://api.hubapi.com/crm/v3/objects/contacts',
         {
             headers: {
                 Authorization: `Bearer pat-na1-2ae0381d-ef96-41d8-8e60-81da0ac163a0`
-            },
-            data: {
-                associations: [
-                    {
-                        types: [
-                            {
-                                "associationCategory": "HUBSPOT_DEFINED",
-                                "associationTypeId": 450
-                            }
-                        ],
-                        to: {
-                            "id": "30750051878"
-                        }
-                    }
-                ],
-                objectWriteTraceId: "string",
-                properties: {
-                    "name": "test"
-                }
-
             }
         }
     ).then(response => res.json(response.data))
