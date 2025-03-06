b7552e52ff5f81d3d9cd0430b370d7c76e2ed79f
pierre schuvey <chromatykretro@gmail.com>
2025-03-06 13:11:14 +0100

    fix

--- src/component/home.js
+++ src/component/home.js
@@ -7,12 +7,7 @@ function HomePage(props) {
     const [myCompanies,setMyCompanies] = useState(null);
     useEffect(() => {
         Axios.get(
-            '/api/getCompaniesHubspot', {
-                headers: {
-                    'authorization': 'Bearer pat-na1-2ae0381d-ef96-41d8-8e60-81da0ac163a0',
-                    'content-type': 'application/json'
-                }
-            }
+            '/api/hello',
         ).then(function(response){
             console.log(response.data);
         })
