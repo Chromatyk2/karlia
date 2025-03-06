173cd2baba65df5edfa64a0bd3d6a8251aaa57d6
pierre schuvey <chromatykretro@gmail.com>
2025-03-06 13:16:04 +0100

    fix

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -11,7 +11,7 @@ router.get("/getCompaniesHubspot", (req, res, next)=>{
         'https://api.hubapi.com/crm/v3/objects/companies',
         {
             headers: {
-                Authorization: `Bearer ${process.env.REACT_APP_CLIENT_SECRET}`
+                Authorization: `Bearer pat-na1-2ae0381d-ef96-41d8-8e60-81da0ac163a0`
             },
         }
     ).then(response => res.json(response.data))
