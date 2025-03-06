8eee68c484099bc32905acf1e8c9ad56312b356e
pierre schuvey <chromatykretro@gmail.com>
2025-03-06 14:47:22 +0100

    fix

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -8,7 +8,7 @@ const router = Router();
 router.get("/hello", (req, res) => res.send("Hello World!"));
 router.get("/createCompanie", (req, res, next)=>{
     Axios.post(
-        'https://api.hubapi.com/crm/v3/objects/contacts',
+        'https://api.hubapi.com/crm/v3/objects/companies',
         {
             headers: {
                 Authorization: `Bearer pat-na1-2ae0381d-ef96-41d8-8e60-81da0ac163a0`
